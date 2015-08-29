cd1d307e78db88f0b50038fa4053801b0dda9b25
gchablowski <g.chablowski@free.fr>
2015-08-29 22:06:42 +0200

    install angular with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fortuneAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fortuneAngularApp
+ */
+angular.module('fortuneAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
